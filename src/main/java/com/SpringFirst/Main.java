3e0505689999a9890d1a6c75fae33596eedf6669
anasmoundir <anasmoundir1996@gmail.com>
2023-11-12 15:06:45 +0100

    testing with the main

--- src/main/java/com/SpringFirst/Main.java
+++ src/main/java/com/SpringFirst/Main.java
@@ -1,13 +1,22 @@
 package com.SpringFirst;
 
 
+import com.Spring.Model.Objectif;
+import com.Spring.Model.Utilisateur;
+import com.Spring.com.Spring.service.ServiceProgressTracker;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
 
     public static   void   main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        Utilisateur utilisateur = (Utilisateur) context.getBean("utilisateur");
+        Objectif objectif = (Objectif) context.getBean("objectif");
+        ServiceProgressTracker progressTracker = new ServiceProgressTracker(utilisateur, objectif);
+        progressTracker.assignGoal();
 
-
-
-}
+    }
 
 }
